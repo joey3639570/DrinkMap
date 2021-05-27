d3d155dedaeabef917ba6bd6d5f2bd7c22521771
joey3639570 <joey3639570@gmail.com>
2021-05-27 16:38:22 +0800

    Initalize django web settings, add .gitignore

---
+++ UI/drinkmapUI/drinkmap/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class DrinkmapConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'drinkmap'
---
+++ UI/env_script.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+source drinkmap/Scripts/activate
