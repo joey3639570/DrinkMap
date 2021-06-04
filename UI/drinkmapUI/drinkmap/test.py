37f2b464e88f3af88f8bcab608b14322e2d37b81
joey3639570 <joey3639570@gmail.com>
2021-06-04 14:00:20 +0800

    Update on Web Query search method.

--- UI/drinkmapUI/drinkmap/test.py
+++ UI/drinkmapUI/drinkmap/test.py
@@ -5,3 +5,4 @@ def dummy(low,high):
     d = {'DRINK': ['black_tea','bool_h'], 'PRICE': [low, high], 'STORE_NAME': ['mr.wish','50blue'], 'STORE_ADDRESS': ['qqq','www'], 'STORE_PHONE': ['0XX','0XX1']}
     df = pd.DataFrame(data=d)
     return df
+
--- UI/drinkmapUI/drinkmap/views.py
+++ UI/drinkmapUI/drinkmap/views.py
@@ -1,21 +1,45 @@
 from django.shortcuts import render
 from .test import dummy
 import json
+
+import os
+import sys
+print(os.getcwd())
+sys.path.append('SQL')
+import sqlMain
 # Create your views here.
 
 def index(request):
     return render(request, 'homepage.html')
 
 def store(request):
+    context = {}
+    input_dict = {}
+    if request.method == 'POST':
+        print(request.POST)
+        if 'new_store_submit' in request.POST:
+            store_name = request.POST.get('store_name')
+            store_address = request.POST.get('store_address')
+            store_phone = request.POST.get('store_phone')
+            input_dict = {'STORE_NAME':store_name, 'STORE_ADDRESS':store_address, 'STORE_PHONE':store_phone}
+            
+        elif 'new_drink_submit' in request.POST:
+            drink_name = request.POST.get('drink')
+            drink_price = request.POST.get('price')
+            input_dict = {'DRINK_NAME':drink_name, 'DRINK_PRICE':drink_price}
+            
+        print(input_dict)
+        
     return render(request, 'store.html')
     
 def user(request):
     context = {}
+    sm = sqlMain.SQLMain()
     if request.method == 'POST':
-        price_high = int(request.POST.get('price_high'))
-        price_low = int(request.POST.get('price_low'))
-        print(request.POST)
-        df = dummy(price_low, price_high)
+        #price_high = int(request.POST.get('price_high'))
+        #price_low = int(request.POST.get('price_low'))
+        #print(request.POST)
+        df = sm.Query(request.POST)
         # parsing the DataFrame in json format.
         json_records = df.to_json(orient ='records')
         data = []
